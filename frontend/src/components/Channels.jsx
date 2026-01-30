8eab5dd275104304a981a40a38f159359d7590c4
Daniel Ivanov <sun.is.still.high@gmail.com>
2026-01-30 18:22:13 +0300

    Add label nname for button

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -62,8 +62,9 @@ const Channels = () => {
                 variant={currentChannelId === channel.id ? 'secondary' : ''}
                 className="w-100 rounded-0 text-start text-truncate"
                 onClick={() => handleChannelClick(channel.id)}
+                aria-label={channel.name}
               >
-                <span className="me-1">#</span>
+                <span className="me-1" aria-hidden="true">#</span>
                 {channel.name}
               </Button>
               {channel.removable && (
